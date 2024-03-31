e33984f442ff4dc94746443d48f49cf0bc128da1
AlexGathecha <alex.gathecha@student.moringaschool.com>
2024-03-31 14:30:13 +0300

    completed labs

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+// Remove the <main> element with id 'main'
+const mainElement = document.getElementById("main");
+mainElement.remove();
+
+// Create a new <h1> element and set its id to 'victory'
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+
+// Set the text content of the new <h1> element
+newHeader.textContent = "John is the champion";
+
+// Append the new <h1> element to the body
+document.body.append(newHeader);
